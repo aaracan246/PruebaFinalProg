c0f5025e150fa23b010b8a84bc91e3c580577041
aaracan246 <acalberto@iesrafaelalberti.es>
2024-03-07 13:28:56 +0100

    Dados - completadov2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ fun main() {
     val diceRoller = DiceRoller()  // probando el dadito
     val d20 = D20()
 
-    val tirada = diceRoller.roll(d20)
+    val roll = diceRoller.roll(d20)
 
-    println(tirada)
+    println(roll)
 }
